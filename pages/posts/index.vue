9b0ab98de04aeff59564f7f4ccf6dc4b3005f26e
Dipesh Parmar <dipeshbeckham@gmail.com>
2020-05-03 11:36:34 +0530

    Pages and component set for posts.

---
+++ pages/posts/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="posts-page">
+    This is posts page.
+  </div>
+</template>
+
+<style scoped>
+  .posts-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
